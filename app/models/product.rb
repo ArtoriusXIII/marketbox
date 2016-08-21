d9c61c1ef986f006490368001a6b4327204359d7
ArtoriusXIII <arthur.janka@gmail.com>
2016-08-21 12:38:23 +0000

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+class Product < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "300x", thumb: "100x100>" }, default_url: "No_Image.png",
+                    :storage => :dropbox,
+                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :name, :description, :price, presence: true
+  
+  belongs_to :user
+  has_many :orders
+end
+    
+
