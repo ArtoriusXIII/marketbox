63a6ad6e73977e432e9aa936c96e84fe4f2d1503
ArtoriusXIII <arthur.janka@gmail.com>
2016-08-21 13:25:21 +0000

    finish setup dropbox

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x", thumb: "100x100>" }, default_url: "No_Image.png",
                     :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                    :path => ":style/:id_:filename"
                 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :name, :description, :price, presence: true
